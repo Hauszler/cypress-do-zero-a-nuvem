74b6372ae2d2de2eea916bd94690c7b90734f264
André Hauszler <andrehauszler@MacBook-Pro-de-Andre.local>
2025-02-06 10:39:13 -0300

    agora eu consigo

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,6 @@ module.exports = defineConfig({
   viewportWidth: 1280,
   e2e: {},
   //video: true,
+  
 })
 
--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -226,7 +226,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.visit('./src/privacy.html')
     cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
   })
-  
+
 })
 
 
